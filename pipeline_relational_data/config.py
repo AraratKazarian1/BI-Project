9f42c1c6e19ef27b512712594a4ec329f5299058
AraratKazarian1 <160218717+AraratKazarian1@users.noreply.github.com>
2024-05-09 18:49:16 +0400

    Commit

--- pipeline_relational_data/config.py
+++ pipeline_relational_data/config.py
@@ -1,5 +1,5 @@
 # Configuration for the pipeline
 
-input_dir = 'C:\\Users\\arara\\OneDrive\\Desktop\\DS-206-GROUP-PROJECT-2\\pipeline_relational_data\\queries'
-sql_server_config = 'C:\\Users\\arara\\OneDrive\\Desktop\\DS-206-GROUP-PROJECT-2\\sql_server_config.cfg'
-log_loc = 'C:\\Users\\arara\\OneDrive\\Desktop\\DS-206-GROUP-PROJECT-2\\logs\\logs_relational_data_pipeline.txt'
+input_dir = 'C:\\Users\\arara\\OneDrive\\Desktop\\DS-206-GROUP-PROJECT-2\\BI-Project\\pipeline_relational_data\\queries'
+sql_server_config = 'C:\\Users\\arara\\OneDrive\\Desktop\\DS-206-GROUP-PROJECT-2\\BI-Project\\sql_server_config.cfg'
+log_loc = 'C:\\Users\\arara\\OneDrive\\Desktop\\DS-206-GROUP-PROJECT-2\\BI-Project\\logs\\logs_relational_data_pipeline.txt'
